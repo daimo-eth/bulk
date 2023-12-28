444fdbc944a06303b83a44bf5adc255b63dcd649
DC <dc@dcpos.ch>
2023-12-27 22:54:54 -0800

    deploy: PerOpInflator to base

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -19,7 +19,7 @@ contract DeployScript is Script {
 
         // Deploy PerOpInflator
         address payable beneficiary = payable(0x2A6d311394184EeB6Df8FBBF58626B085374Ffe7);
-        PerOpInflator pi = new PerOpInflator{salt:0}(msg.sender);
+        PerOpInflator pi = new PerOpInflator{salt:"01"}(msg.sender);
         pi.setBeneficiary(beneficiary);
 
         // Deploy DaimoOpInflator
@@ -27,15 +27,15 @@ contract DeployScript is Script {
         address paymaster;
         if (block.chainid == 8453) {
             tokenAddress = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913; // Base USDC
-            paymaster = 0xedb395b8BD78788A57e3C8eD9b748f9CC29C2864; // Daimo Sub Paymaster (uses Metapaymaster)
+            paymaster = 0xac5917075b3ED3a6a4516398800f3f64FCf4631E; // DaimoPaymasterV2
         } else if (block.chainid == 84531){
             tokenAddress = 0x1B85deDe8178E18CdE599B4C9d913534553C3dBf; // Base Goerli testUSDC
-            paymaster = 0x6f0F82fAFac7B5D8C269B02d408F094bAC6CF877; // DaimoPaymaster
+            paymaster = 0x13f490FafBb206440F25760A10C21A6220017fFa; // Pimlico ERC20 paymaster
         } else {
             revert("Unsupported chain");
         }
 
-        DaimoOpInflator i = new DaimoOpInflator{salt:0}(tokenAddress, msg.sender);
+        DaimoOpInflator i = new DaimoOpInflator{salt:"01"}(tokenAddress, msg.sender);
         i.setPaymaster(paymaster);
 
         // Register DaimoOpInflator
